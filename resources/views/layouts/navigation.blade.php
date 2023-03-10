0c3a563ae9074c6bcd0745aea214c9ffad27ffa7
eucn <eubertnovencido20@gmail.com>
2023-03-10 14:54:51 +0800

    new guest updates

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
 
 <nav x-data="{ open: false }" class="bg-[#51BDB8] h-[75px]">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-1">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo --> 
